3f66738db07bf7ace0d444720c7f0d1c1227214e
Soulmate13 <glebzhurba@gmail.com>
2019-11-20 17:03:20 +0200

    started task 2

---
+++ script_2.js
@@ -0,0 +1,53 @@
+// 2. Создать массив, описывающий чек в магазине. Каждый элемент массива состоит из названия товара, количества и цены за единицу товара. Написать следующие функции:
+let cheque = [{
+    "name": "Apples",
+    "count": 3,
+    "price": 30
+}, {
+    "name": "Pears",
+    "count": 4,
+    "price": 25
+}, {
+    "name": "Beetroot",
+    "count": 3,
+    "price": 40
+}
+];
+// Распечатка чека на экран;
+
+let chequePrint = function (_cheque) {
+
+    let _buff = `<ul>`
+    for (const key in _cheque) {
+        if (typeof _cheque[key] == "object") {
+            _buff += `<li> ${key.toUpperCase()} : ${chequePrint(_cheque[key])}</li>`
+        } else {
+            _buff += `<li> ${key}:${_cheque[key]} </li>`
+        }
+    }
+    _buff += `</ul>`;
+    return _buff;
+};
+
+let DisplayCheque = function () {
+    document.getElementById("desk").innerHTML = `<pre>${chequePrint(cheque)}</pre>`;
+};
+// Подсчет общей суммы покупки;
+
+let chequeCount = function (_cheque) {
+
+    let sum = 0;
+
+    for (i = 0; i < _cheque.length; i++) {
+        sum += _cheque[i].price * _cheque[i].count;
+    };
+
+    document.getElementById("desk2").innerHTML = `<pre>The sum is ${sum}</pre>`
+
+}
+
+// Получение самой дорогой покупки в чеке;
+
+
+
+// Подсчет средней стоимости одного товара в чеке.
